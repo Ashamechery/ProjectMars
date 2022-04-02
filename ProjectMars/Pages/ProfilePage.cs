358fc2b6841f95ac89ffc486f5a2c6c3ec68da9f
Ashamechery <ashamechery11@gmail.com>
2022-04-02 16:58:21 +1300

    Test Automated using POM Model

--- ProjectMars/Pages/ProfilePage.cs
+++ ProjectMars/Pages/ProfilePage.cs
@@ -1,49 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using ProjectMars.Utilities;
 
-namespace ProjectMars
+namespace ProjectMars.Pages
 {
-    internal class Program
+    internal class ProfilePage
     {
-        static void Main(string[] args)
+        public void CreateProfileDetails(IWebDriver driver)
         {
-            //open the chrome browser
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            //launch turnup  portal
-            driver.Navigate().GoToUrl("http://localhost:5000/");
-
-            //click on signin
-            IWebElement signinButton = driver.FindElement(By.XPath("//*[@id='home']/div/div/div[1]/div/a"));
-            signinButton.Click();
-            //Identify user name text box and enter valid user name
-            IWebElement usernameTextbox = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[1]/input"));
-            usernameTextbox.SendKeys("ashamechery11@gmail.com");
-
-            //identify password and enter valid password
-            IWebElement passwordTextbox = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[2]/input"));
-            passwordTextbox.SendKeys("Keziahshaji@123#");
-            //click on login button
-            IWebElement loginButton = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button"));
-            loginButton.Click();
-            Thread.Sleep(5000);
-            //check if i successfully loggedin
-            IWebElement helloAsha = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/div[1]/div[2]/div/span"));
-            Thread.Sleep(5000);
-            if (helloAsha.Text == "Hi Asha")
-            {
-                Console.WriteLine("login passed");
-            }
-            else
-            {
-                Console.WriteLine("login failed");
-            }
             //adding languages
             //clink on add new for adding languages
             IWebElement addNew = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/thead/tr/th[3]/div"));
             addNew.Click();
+            Wait.WaitToBeClickable(driver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[1]/input", 2);
+
             // type the language
             IWebElement addLanguage = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[1]/input"));
             addLanguage.SendKeys("malayalam");
@@ -55,10 +30,10 @@ namespace ProjectMars
             basicLevelAdd.Click();
             IWebElement add = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[3]/input[1]"));
             add.Click();
-            Thread.Sleep(2000);
+            Wait.WaitToBeClickable(driver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td[2]", 2);
             //Check if the data is created or not
             IWebElement createdData = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td[2]"));
-            if (createdData.Text == "Basic" )
+            if (createdData.Text == "Basic")
 
             {
                 Console.WriteLine("Language created successfully");
@@ -67,21 +42,24 @@ namespace ProjectMars
             {
                 Console.WriteLine("Language not created successfully");
             }
+        }
+        public void EditProfileDetails(IWebDriver driver)
+        {
             //Edting created element
             //click on edit icon
             IWebElement editButton = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[1]/tr/td[3]/span[1]"));
             editButton.Click();
             //update inside 'add anguage' text box
-            IWebElement addLangupdate= driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td/div/div[1]/input"));
+            IWebElement addLangupdate = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td/div/div[1]/input"));
             addLangupdate.Clear();
             addLangupdate.SendKeys("Hindi");
             //click the level as fluent
-             IWebElement chooseLevelupdate = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td/div/div[2]/select/option[4]"));
-             chooseLevelupdate.Click();
+            IWebElement chooseLevelupdate = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td/div/div[2]/select/option[4]"));
+            chooseLevelupdate.Click();
             //click on update 
             IWebElement updateButton = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td/div/span/input[1]"));
             updateButton.Click();
-            Thread.Sleep(2000);
+            Wait.WaitToBeClickable(driver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td[2]", 2);
             //check if created data is updated
             IWebElement editedData = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td[2]"));
             if (editedData.Text == "Fluent")
@@ -93,11 +71,15 @@ namespace ProjectMars
             {
                 Console.WriteLine("created Language not edited successfully");
             }
+        }
+        public void DeleteProfileDetails(IWebDriver driver)
+        {
             //Delete created lang
             IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td[3]/span[2]/i"));
             deleteButton.Click();
+            IWebElement editedData = driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td[2]"));
 
-            if(editedData.Text=="fluent")
+            if (editedData.Text == "fluent")
             {
                 Console.WriteLine("created language not removed-Test failed");
 
---
+++ ProjectMars/Tests/ProfileDetails_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ProjectMars.Pages
+{
+    internal class ProfileDetails_Tests
+    {
+        static void Main(string[] args)
+        {
+            //open the chrome browser
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            //Login page object initialization and definition
+            LoginPage loginPageObj = new LoginPage();  
+            loginPageObj.LoginSteps(driver);
+
+            //Profile page object initialization and definition
+            ProfilePage profilePageObj=new ProfilePage();
+            profilePageObj.CreateProfileDetails(driver);
+            //edit
+            profilePageObj.EditProfileDetails(driver);
+            //delete
+            profilePageObj.DeleteProfileDetails(driver);
+        }
+    }
+}
